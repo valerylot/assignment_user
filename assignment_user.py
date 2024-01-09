9b9d72e8521294881dff2b1d3ae28c014d855f9e
valerylot <lotvalery@gmail.com>
2024-01-09 15:30:05 -0800

    chaining methods

--- assignment_user.py
+++ assignment_user.py
@@ -14,6 +14,7 @@ class User:
         print(f"Age: {self.age}")
         print(f"Member: {self.is_rewards_member}")
         print(f"Current Points: {self.gold_card_points}")
+        return self
 
     def enroll(self):
         if self.is_rewards_member == True:
@@ -21,12 +22,14 @@ class User:
         else:
             self.is_rewards_member = True
             self.gold_card_points = 200
+        return self
     
     def spend_points(self, amount):
         if amount < self.gold_card_points:
             self.gold_card_points -= amount
         else:
             print("Not enough points!")
+        return self
 
 
 #create some user
@@ -34,24 +37,26 @@ user1=User("Jane", "Doe", "janedoe@gmail.com", "35")
 user2=User("Bob", "Builder", "bob@gmail.com", "45")
 user3=User("Kanye", "East", "east@gmail.com", "25")
 
-print(user1.first_name)
-user1.display_info()
+# print(user1.first_name)
+# user1.display_info()
 
-user1.enroll()
-user1.display_info()
+# user1.enroll()
+# user1.display_info()
 
-user1.spend_points(50)
-user1.display_info()
+# user1.spend_points(50)
+# user1.display_info()
 
-user2.enroll()
-user2.display_info()
+# user2.enroll()
+# user2.display_info()
 
-user2.spend_points(80)
-user2.display_info()
+# user2.spend_points(80)
+# user2.display_info()
 
-user1.display_info()
-user2.display_info()
-user3.display_info()
+# user1.display_info()
+# user2.display_info()
+# user3.display_info()
 
-user3.spend_points(40)
-user3.display_info
+# user3.spend_points(40)
+# user3.display_info
+
+user1.display_info().enroll().spend_points(150).display_info()
